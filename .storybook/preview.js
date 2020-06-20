8b8ddd85a2193aeea5fdacd8dc1cc5f84d7c4353
tiny <tiny0918@126.com>
2020-06-20 14:47:45 +0800

    fix:ci

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,31 +1,13 @@
-import { configure, addDecorator, addParameters } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info'
-import React from 'react'
-import { create } from '@storybook/theming'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import "../src/styles/index.scss"
-// add-info table style not working for now, I don't know the reason, so we have to manaul import it
-import "./fix_info_style.scss"
-
-library.add(fas)
-const wrapperStyle = {
-  padding: '20px 40px',
-  width: '600px'
-}
-
-const storyWrapper = (stroyFn) => (
-  <div style={wrapperStyle}>
-    {stroyFn()}
-  </div>
-)
-addDecorator(storyWrapper)
-addDecorator(withInfo)
+import "../src/styles/index.scss";
+import { create } from "@storybook/theming";
+import { addParameters, configure } from "@storybook/react";
+import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
 addParameters({
-  info: { 
-      inline: true, 
-      header: false
-  },
+	docs: {
+		container: DocsContainer,
+		page: DocsPage,
+		PreviewSource: "open"
+	},
   options: {
       theme: create({
           brandTitle: 'tintui',
